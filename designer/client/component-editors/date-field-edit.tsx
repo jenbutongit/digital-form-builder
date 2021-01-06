77afc934e2ce9db8164f9457361445e8794ffb01
bertrandmc <bemacode@gmail.com>
2021-01-06 17:59:33 +0000

    missing refactor

--- designer/client/component-editors/date-field-edit.tsx
+++ designer/client/component-editors/date-field-edit.tsx
@@ -10,7 +10,7 @@ type Props = {
 export function DateFieldEdit({ context = ComponentContext }: Props) {
   // If you are editing a component, the default context will be ComponentContext because props.context is undefined,
   // but if you editing a component which is a children of a list based component, then the props.context is the ListContext.
-  const [state, dispatch] = useContext(context);
+  const { state, dispatch } = useContext(context);
   const { selectedComponent } = state;
   const { options } = selectedComponent;
 
