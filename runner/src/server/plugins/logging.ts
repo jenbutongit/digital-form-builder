468dbec0c2a11dc09835d80ad75d1e264dd3199b
jenbutongit <jen+git@cautionyourblast.com>
2021-06-18 13:04:57 +0100

    go away asset logs

--- runner/src/server/plugins/logging.ts
+++ runner/src/server/plugins/logging.ts
@@ -8,10 +8,20 @@ export default {
     level: "trace",
     ignorePaths: [
       "/assets",
+      "/assets/*",
       "/assets/images/favicon.ico",
       "/assets/fonts/light-94a07e06a1-v2.woff2",
+      "/assets/fonts/bold-affa96571d-v2.woff",
+      "/assets/fonts/light-f591b13f7d-v2.woff",
       "/assets/images/govuk-crest-2x.png",
       "/assets/all.js",
+      "/assets/upload-dialog.js",
+      "/assets/jquery-3.5.1.min.js",
+      "/assets/accessible-autocomplete.min.js.map",
+      "/assets/accessible-autocomplete.min.js",
+      "/assets/stylesheets/application.css",
+      "/assets/dialog-polyfill.0.4.3.js",
+      "/assets/govuk-template.js",
     ],
     formatters: {
       level: (label) => {
